0655a7b73cab79e22cabbd9fa14ea40d521205c4
mauricio.andrade <mandrade_pr@yahoo.com.br>
2025-07-18 15:03:26 -0300

    feat:mensageria para envio de pedidos criados.

--- src/main/java/br/com/cotiinformatica/domain/services/impl/PedidoServiceImpl.java
+++ src/main/java/br/com/cotiinformatica/domain/services/impl/PedidoServiceImpl.java
@@ -4,26 +4,65 @@ import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import br.com.cotiinformatica.domain.entities.Pedido;
+import br.com.cotiinformatica.domain.events.PedidoCriadoEvent;
 import br.com.cotiinformatica.domain.exceptions.PedidoNaoEncontradoException;
 import br.com.cotiinformatica.domain.models.PedidoRequestModel;
 import br.com.cotiinformatica.domain.models.PedidoResponseModel;
 import br.com.cotiinformatica.domain.services.interfaces.PedidoService;
+import br.com.cotiinformatica.infrastructure.outbox.OutboxMessage;
+import br.com.cotiinformatica.infrastructure.repositories.OutboxMessageRepository;
 import br.com.cotiinformatica.infrastructure.repositories.PedidoRepository;
 import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class PedidoServiceImpl implements PedidoService {
 	private final PedidoRepository pedidoRepository;
+	private final OutboxMessageRepository outboxMessageRepository;
 	private final ModelMapper mapper;
+	private final ObjectMapper objectMapper;
 	
+	@Transactional
 	@Override
 	public PedidoResponseModel criarPedido(PedidoRequestModel model) {
 	
+		//Capturando os dados da requisição e criando um novo pedido
 		var pedido = mapper.map(model, Pedido.class);
 				
+		//Salvando o pedido no banco de dados
 		pedidoRepository.save(pedido);
 		
+		//Criando um evento "PedidoCriado" com os dados do pedido
+		var event = new PedidoCriadoEvent(
+				pedido.getId(),
+				pedido.getDataPedido(),
+				pedido.getValorPedido(),
+				pedido.getNomeCliente(),
+				pedido.getDescricaoPedido(),
+				pedido.getStatus().toString()
+				);
+		
+		//Criando um registro para a tabela de saída (OutboxMessage)
+		var message = new OutboxMessage();
+		message.setAggregateType("Pedido"); //nome da entidade
+		message.setAggregateId(pedido.getId().toString()); //id do pedido
+		message.setType("PedidoCriado"); //nome do evento
+		
+		try {
+			//Serializando os dados do evento em JSON
+			message.setPayload(objectMapper.writeValueAsString(event));
+		}
+		catch(JsonProcessingException e) {
+			throw new IllegalStateException(e.getMessage());
+		}
+		
+		//Salvando o registro do evento no banco de dados
+		outboxMessageRepository.save(message);
+		
+		//Retornando os dados do pedido cadastrado
 		return mapper.map(pedido, PedidoResponseModel.class);
 	}
 	@Override
